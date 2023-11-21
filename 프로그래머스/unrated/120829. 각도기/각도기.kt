f1259f2a030f683b5f634cf1f92b11ace943bf90
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-11-21 10:30:34 +0900

    [level unrated] Title: 각도기, Time: 0.01 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(angle: Int): Int {
+        var answer: Int = 0
+        when(angle) {
+            in 0 .. 89 -> answer = 1
+            90 -> answer = 2
+            in 91 .. 179 -> answer = 3
+            180 -> answer = 4
+        }
+        return answer
+    }
+}
