1321baf333ade04dfe96a5cc88d7fc26b44fa917
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-11-21 09:52:42 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num1: Int, num2: Int) = num1 * 1000 / num2
+    
+    // fun solution(num1: Int, num2: Int): Int {
+    //     var answer: Int = 0
+    //     val d_num1 = num1.toDouble()
+    //     val d_num2 = num2.toDouble()
+    //     answer = (d_num1 / d_num2 * 1000).toInt()
+    //     return answer
+    // }
+}
