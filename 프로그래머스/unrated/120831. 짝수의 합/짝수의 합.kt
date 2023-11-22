5ac975fe70d5ecf352454c38ad6967c439006045
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-11-22 09:31:34 +0900

    [level unrated] Title: 짝수의 합, Time: 8.99 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.kt
@@ -1,8 +1,10 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer: Int = 0
-        for(i: Int in 2..n step(2))
-             answer += i    
-        return answer
-    }
+    fun solution(n: Int): Int =  (0..n step 2).sum()
+        //1.
+        // (2..n).filter{ it % 2 == 0}.sum()
+    
+        //2.내가 한 거
+        // for(i: Int in 2..n step(2))
+        //      answer += i    
+     
 }
