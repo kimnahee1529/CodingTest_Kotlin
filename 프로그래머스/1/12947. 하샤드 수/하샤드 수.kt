e190944ff004a1628e64518c8fcec8ebf314355c
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-11-30 23:29:18 +0900

    [level 1] Title: 하샤드 수, Time: 5.75 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,8 +1,19 @@
 class Solution {
     fun solution(x: Int): Boolean {
-        if(x % x.toString().map{it.toString().toInt()}.sum() == 0)
-            return true
-        else
-            return false
+        val xtoString = x.toString()
+        val sum = xtoString.map{
+            it.toString().toInt()
+        }.sum()
+        
+        if(x % sum == 0) return true else return false
+        
+      
+        // if(x % x.toString().map{it.toString().toInt()}.sum() == 0)
+        //     return true
+        // else
+        //     return false
     }
 }
+//문자열로 바꾼다
+//각 자리수를 더한다
+//나눠지는지 확인한다
