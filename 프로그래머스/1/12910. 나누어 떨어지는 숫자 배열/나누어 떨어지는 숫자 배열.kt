8f08a4abd48fa15a96c1cf164950012caf0b7769
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-11-30 11:35:33 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 26.18 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.kt
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.kt
@@ -1,12 +1,13 @@
 class Solution {
     fun solution(arr: IntArray, divisor: Int): IntArray {
-        var answer = intArrayOf()
+        val answer = mutableListOf<Int>()
         for(i in arr){
             if(i % divisor == 0){
-                println(i)
-                answer.plus(i)
+                answer.add(i)
             }
         }
-        return answer
+       if(answer.isEmpty())
+            answer.add(-1)
+        return answer.sorted().toIntArray()
     }
 }
