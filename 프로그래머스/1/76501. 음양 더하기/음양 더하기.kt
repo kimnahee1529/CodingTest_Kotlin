e97597a38a3bf5272a64960546efa878f9b7428d
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-01 23:37:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
@@ -1,14 +1,6 @@
 class Solution {
     fun solution(absolutes: IntArray, signs: BooleanArray): Int {
-        var answer: Int = 0
-
-        signs.forEachIndexed { index, value ->  
-            if(value)
-                answer += absolutes[index]
-            else
-                answer -= absolutes[index]
-        }
-        
+        var answer: Int = 123456789
         return answer
     }
 }
