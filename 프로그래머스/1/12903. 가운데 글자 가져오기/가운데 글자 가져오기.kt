9903ba7ce664ea6383222ebfcec6a00fa3e44f14
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-07 22:34:52 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 21.75 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -1,12 +1,6 @@
 class Solution {
     fun solution(s: String): String {
         var answer = ""
-        val stringLength = s.length/2
-        if(s.length % 2 == 0)
-            answer += (s[stringLength-1].toString() + s[stringLength].toString())
-        else
-            answer += s[stringLength].toString()
-
         return answer
     }
 }
