3f0ed774480099a4a7506cfe3d4905f8f967ecf9
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-08 09:28:42 +0900

    [level 1] Title: 내적, Time: 0.03 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.kt
+++ 프로그래머스/1/70128. 내적/내적.kt
@@ -1,6 +1,10 @@
 class Solution {
     fun solution(a: IntArray, b: IntArray): Int {
-        var answer: Int = 1234567890
+        var answer: Int = 0
+        val length = a.size
+        for(i in 0..length-1){
+            answer += a[i] * b[i]
+        }
         return answer
     }
 }
