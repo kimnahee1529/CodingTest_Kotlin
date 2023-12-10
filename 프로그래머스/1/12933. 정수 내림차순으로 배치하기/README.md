189c5e375a7f0090752c24bbbcb71e1bfbfbb3ab
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-10 15:02:17 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 21.95 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
-# ⭐[level 1] 정수 내림차순으로 배치하기 - 12933 
+# [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 62.9 MB, 시간: 14.87 ms
+메모리: 64.2 MB, 시간: 21.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 10:19:43
+2023년 12월 0일 15:2:15
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.kt
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.kt
@@ -1,8 +1,8 @@
 class Solution {
-    fun solution(n: Long): Long = n.toString().toCharArray().sortedArrayDescending().joinToString("").toLong()
-}
+    fun solution(n: Long): Long {
+        var answer: Long = 0
+        answer = n.toString().map{ it.toString().toLong()}.sortedDescending().joinToString("").toLong()
 
-// println(n.toString())
-        // println(n.toString().toCharArray())
-        // println(n.toString().toCharArray().sortedArray()) //오름차순 정렬
-        // println(n.toString().toCharArray().sortedArrayDescending().joinToString("").toLong()) //내림차순 정렬
+        return answer
+    }
+}
