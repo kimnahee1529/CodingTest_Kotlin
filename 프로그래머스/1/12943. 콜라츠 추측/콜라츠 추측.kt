0011d1a240326520899aa6eb96d19e3813873794
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-10 18:41:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun solution(num: Int): Int {
-        var n: Long  = num.toLong()
-        var answer = 0
-        while(answer<500){
-            if(n == 1L){
-                return answer
-            }
-            else{
-                if(n%2==0L){
-                    n = n / 2
-                }else{
-                    n = n * 3 + 1
-                }
-            }
-            answer = answer + 1
+    var number = num.toLong()
+    var answer = 0
+    while(answer<500){
+        if(number == 1L){
+            return answer
         }
-        answer = -1
-        return answer
+        else{
+            if(number%2==0L){
+                number /= 2
+            }else{
+                number = number * 3 + 1
+            }
+        }
+        answer++
+    }
+    answer = -1
+    return answer
     }
 }
