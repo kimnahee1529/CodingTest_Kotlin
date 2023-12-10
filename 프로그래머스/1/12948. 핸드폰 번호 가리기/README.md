3e4f402f58dc12438712f0716bee5d752690492f
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-10 20:26:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 13.00 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948) 
 
 ### 성능 요약
 
-메모리: 60.8 MB, 시간: 17.31 ms
+메모리: 61.1 MB, 시간: 13.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 9:53:35
+2023년 12월 0일 20:26:12
 
 ### 문제 설명
 
--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,9 +1,11 @@
 class Solution {
     fun solution(phone_number: String): String {
         var answer = ""
-        val number_length = phone_number.length
-        val last_number_length = 4
-        answer = answer.padStart(number_length-last_number_length,'*') + phone_number.takeLast(last_number_length)
+        val len = phone_number.length-4
+        repeat(len){
+            answer += "*"
+        }
+        answer += phone_number.takeLast(4)
         return answer
     }
 }
