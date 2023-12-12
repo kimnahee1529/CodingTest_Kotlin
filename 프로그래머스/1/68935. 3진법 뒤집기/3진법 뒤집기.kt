d0b58e5f80274f62c2bc11ad49aea0efa2b9cca5
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-12 23:32:33 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.19 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        val ternaryString = n.toString(3)
+        val reversedTernary = ternaryString.reversed()
+        answer = reversedTernary.toInt(3)
+        return answer
+    }
+}
