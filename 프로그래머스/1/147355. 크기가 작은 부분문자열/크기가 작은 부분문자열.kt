03962995623102324486265372adc0a99fdf5d08
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-14 10:23:57 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 9.60 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun solution(t: String, p: String): Int {
         var answer: Int = 0
-    val tLen = t.length-1
-    val pLen = p.length-1
-    for(i in 0..tLen-pLen){
-        var num = t.slice(i..i+pLen)
-        if(num.toLong() <= p.toLong()){
-            answer++
+        for(i in 0..t.length-p.length){
+            var num = t.slice(i..i+p.length-1)
+            if(num.toLong() <= p.toLong()){
+                answer++
+            }
         }
-    }
     return answer
     }
 }
