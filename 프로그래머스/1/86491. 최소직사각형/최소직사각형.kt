e567d4baa9a7fc17eff2717a0704d41109e0bc57
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-15 09:51:11 +0900

    [level 1] Title: 최소직사각형, Time: 28.44 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -10,15 +10,11 @@ class Solution {
         val maxList = sizes.map {
             it.maxOrNull() ?: 0
         }
-        val min = minList.maxOrNull() ?: 0
-        val max = maxList.maxOrNull() ?: 0
+  
+        val firstMax = minList.maxOrNull() ?: 0
+        val secondMax = maxList.maxOrNull() ?: 0
 
-        println("minList: $minList")
-        println("maxList: $maxList")
-        println("min: $min, max: $max")
-
-        answer = min * max
-        println(answer)
+        answer = firstMax * secondMax
         return answer
     }
 }
