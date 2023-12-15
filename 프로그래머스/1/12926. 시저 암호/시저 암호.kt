c53a2a3720406ffe2e703cb3f5a410c2230418d8
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-15 11:15:34 +0900

    [level 1] Title: 시저 암호, Time: 35.47 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun solution(s: String, n: Int): String {
         var answer = ""
-        val uppercase = listOf('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-        val lowercase = listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-        
+        val uppercase = ('A'..'Z').toList()
+        val lowercase = ('a'..'z').toList()
+
         val result = s.map {
             if(it.isUpperCase()){
                 println(uppercase.indexOf(it)+n)
