b33d3fa280a0f442d365e84ca40b4f9edc3d2f4c
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-25 22:46:45 +0900

    [level 1] Title: 모의고사, Time: 11.43 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/1/42840. 모의고사/모의고사.kt
@@ -1,6 +1,34 @@
+import java.util.Collections.max
 class Solution {
     fun solution(answers: IntArray): IntArray {
-        var answer = intArrayOf()
-        return answer
+        val answer = mutableListOf<Int>()
+        val patterns = arrayOf(
+            intArrayOf(1, 2, 3, 4, 5),
+            intArrayOf(2, 1, 2, 3, 2, 4, 2, 5),
+            intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        )
+        val score = mutableListOf(0,0,0)
+    
+        answers.forEachIndexed { index, i ->
+            val indexA = index % patterns[0].size
+            val indexB = index % patterns[1].size
+            val indexC = index % patterns[2].size
+            if(i == patterns[0][indexA]){
+                score[0]++
+            }
+            if(i == patterns[1][indexB]){
+                score[1]++
+            }
+            if(i == patterns[2][indexC]){
+                score[2]++
+            }
+        }
+    
+        score.forEachIndexed { index, i ->
+            if(i == max(score)){
+                answer.add(index+1)
+            }
+        }
+        return answer.toIntArray()
     }
 }
