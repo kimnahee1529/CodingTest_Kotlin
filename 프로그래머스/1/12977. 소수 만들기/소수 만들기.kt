f189fadc1b15dee5adff8da9002efcc970ceb97c
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-31 00:38:39 +0900

    [level 1] Title: 소수 만들기, Time: 0.29 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(nums: IntArray): Int {
+        var answer = 0
+        for(i in 0 until nums.size-2){
+            for(j in i+1 until nums.size-1){
+                for(k in j+1 until nums.size){
+                    if(isPrime(nums[i] + nums[j] + nums[k]))
+                        answer+=1
+                }
+            }
+        }
+        return answer
+    }
+}
+fun isPrime(x: Int): Boolean {
+    for (i in 2 until x) {	// 2부터 x-1까지 반복
+        if (x % i == 0) return false	// x가 한 번이라도 나누어 떨어지면 소수가 아님
+    }
+    return true	// 모두 반복해도 나누어 떨어지지 않으면 소수
+}
