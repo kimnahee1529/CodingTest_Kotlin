8c9558f1d86754d37292effe18c21788da059b2f
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2023-12-31 00:40:59 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -11,10 +11,11 @@ class Solution {
         }
         return answer
     }
-}
-fun isPrime(x: Int): Boolean {
-    for (i in 2 until x) {	// 2부터 x-1까지 반복
-        if (x % i == 0) return false	// x가 한 번이라도 나누어 떨어지면 소수가 아님
+    
+    fun isPrime(x: Int): Boolean {
+        for (i in 2 until x) {	// 2부터 x-1까지 반복
+            if (x % i == 0) return false	// x가 한 번이라도 나누어 떨어지면 소수가 아님
+        }
+        return true	// 모두 반복해도 나누어 떨어지지 않으면 소수
     }
-    return true	// 모두 반복해도 나누어 떨어지지 않으면 소수
 }
