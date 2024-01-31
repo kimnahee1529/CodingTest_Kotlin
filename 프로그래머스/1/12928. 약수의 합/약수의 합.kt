0c0f1283d89a47fe50ec9bfef8ce40598aaa71b1
hyihyi <65105035+kimnahee1529@users.noreply.github.com>
2024-01-31 23:37:05 +0900

    Update 약수의 합.kt

--- 프로그래머스/1/12928. 약수의 합/약수의 합.kt
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.kt
@@ -11,17 +11,12 @@ class Solution {
                 answerList.add(i)
             }
         }
-        // println(answerList)
         
         for (i in 0..answerList.size-1){
             if(n % answerList[i] == 0){
                 answerList.add(n / answerList[i])
             }
         }
-
-        // answerList = answerList.distinct()
-        // println(answerList)
-        // println(answerList.distinct())
         
         return answerList.distinct().sum()
     }
